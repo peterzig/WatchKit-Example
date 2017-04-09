2f885d6ee6b1d934abc526f16d37397c383845cc
Peter Zaporowski <peter@Peters-Mac.local>
2017-04-09 22:54:18 +0200

    first commit of first Apple Watch App

---
+++ WatchKitApp Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  WatchKitApp Extension
+//
+//  Created by Peter Zaporowski on 09.04.2017.
+//  Copyright © 2017 Peter Zaporowski. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
